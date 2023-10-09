a6738c724e10f7e89a5fd1e8a41ae2de0029b9ee
Abdur Rehman Kazim <abdurrehmankazim68@gmail.com>
2023-10-09 14:32:15 +0500

    Working on it!

---
+++ src/components/Slider.jsx
@@ -0,0 +1,17 @@
+import { Carousel } from 'bootstrap'
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className='d-block w-100'
+          src=""
+          alt="first slide" />
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+export default Slider
